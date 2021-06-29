af2cd60b897f5f9f0f215d3360ec15563bf2ffa0
Cory McCartan <cory.mccartan@gmail.com>
2021-06-29 15:57:09 -0400

    edits

--- render_rmd.R
+++ render_rmd.R
@@ -10,3 +10,16 @@ render_site_post = function(input, ..., site_path="~/Documents/Code/website/cont
     file.copy("figures/", paste0(out_path), recursive=T)
     file.remove("index.md")
 }
+
+render_site_proj = function(input, ..., site_path="~/Documents/Code/website/content/projects/") {
+    fm = rmarkdown::yaml_front_matter(input)
+    slug = if (!is.null(fm$slug)) fm$slug else xfun::sans_ext(basename(input))
+    out_path = paste0(site_path, format(Sys.Date(), "%Y-%m-"), slug, "/")
+    if (!dir.exists(out_path)) dir.create(out_path, recursive=T)
+    if (!dir.exists("figures/")) dir.create("figures/")
+    out_file = paste0(out_path, "index.md")
+    rmarkdown::render(input, output_file="index.md")
+    file.copy("index.md", paste0(out_path, "index.md"), overwrite=T)
+    file.copy("figures/", paste0(out_path), recursive=T)
+    file.remove("index.md")
+}
